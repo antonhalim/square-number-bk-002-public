9ca1c25af058c5173833b30ca992ccb403cb6628
axton21 <axton_n@yahoo.com>
2015-03-04 00:01:57 -0500

    done

--- lib/square_number.rb
+++ lib/square_number.rb
@@ -1,5 +1,15 @@
+require 'pry'
 class Integer
+
   def square_number?
-    # code goes here
+    number = 0
+    until number**2 > self do
+      number += 1
+      if number**2 == self
+        return number
+      end
+    end
+    false
   end
+
 end
